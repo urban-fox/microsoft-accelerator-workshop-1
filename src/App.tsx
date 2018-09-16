affcd44aa886d8f5107c42371e4a52067ae42df5
Jargil Santos <urban-fox@users.noreply.github.com>
2018-09-16 14:14:18 +1200

    Installed dropzone, spinner, added sort keys rule to tslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import * as React from 'react';
+import Dropzone from 'react-dropzone'
+import Loader from 'react-loader-spinner'
 import './App.css';
+
 
 export default class App extends React.Component<{}> {
   public render() {
